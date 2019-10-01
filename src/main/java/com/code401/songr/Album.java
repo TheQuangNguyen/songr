1f312c70147e789a3c1efebd6724dc30c1793969
Quang Nguyen <quangn2595@gmail.com>
2019-10-01 07:52:38 -0700

    checkpoint figuring out how to serve static files

--- src/main/java/com/code401/songr/Album.java
+++ src/main/java/com/code401/songr/Album.java
@@ -6,4 +6,12 @@ public class Album {
     int songCount;
     int length;
     String imageURL;
+
+    public Album(String title, String artist, int songCount, int length, String imageURL) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageURL = imageURL;
+    }
 }
