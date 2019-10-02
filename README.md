c4ec7c3580039c558de9caabe44dbcb3bb426874
Quang Nguyen <quangn2595@gmail.com>
2019-10-02 11:59:04 -0700

    got adding album functionality to the website

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Songr App
 
-**Work In Progress**
+**Work In Progress - adding update and delete albums functionality**
 
-This is a web app that stores and displays your favorite albums. That's about it currently.  
+This is a web app that stores and displays your favorite albums. You can add new albums on to your storage by filling out the form on the front page and click "Add Album" button. You can see your stored albums by clicking on the "My Albums" nav link. 
--- src/main/java/com/code401/songr/controllers/AlbumsController.java
+++ src/main/java/com/code401/songr/controllers/AlbumsController.java
@@ -1,13 +1,21 @@
-package com.code401.songr;
+package com.code401.songr.controllers;
 
+import com.code401.songr.models.Album;
+import com.code401.songr.models.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 public class AlbumsController {
 
-    Album[] albums = new Album[3];
+    @Autowired
+    AlbumRepository albumRepository;
     Album album1 = new Album("Map of the Soul: Persona",
                             "BTS",
                             7,
@@ -26,10 +34,20 @@ public class AlbumsController {
 
     @GetMapping("/albums")
     public String getAlbumsPage(Model m) {
-        albums[0] = album1;
-        albums[1] = album2;
-        albums[2] = album3;
+//        albums[0] = album1;
+//        albums[1] = album2;
+//        albums[2] = album3;
+
+        List<Album> albums = albumRepository.findAll();
         m.addAttribute("albums", albums);
         return "albums";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addAlbums(String title, String artist, int songCount,  double length, String imageURL) {
+        Album newAlbum = new Album(title, artist, songCount, length, imageURL);
+        albumRepository.save(newAlbum);
+
+        return new RedirectView("/albums");
     }
 }
